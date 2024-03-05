151b4c8b4cf72dcca1de552fb943cc5a58ec0ae6
Ítalo Ramalho <italo.ramalho@ccc.ufcg.edu.br>
2024-03-05 00:06:51 -0300

    adding spring init

---
+++ src/main/java/com/alugai/demo/AlugaiApplication.java
@@ -0,0 +1,13 @@
+package com.alugai.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alugai/demo/AlugaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alugai.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
